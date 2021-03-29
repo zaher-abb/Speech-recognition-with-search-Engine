f5847a2f2c3242e72af1f8519f2223e1e4a96b1f
zaher-abb <abboudz@th-brandenburg.de>
2021-03-29 19:16:40 +0200

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,6 +4,9 @@ import speech_recognition as sr
 import wavio
 import re
 from googlesearch import search
+from nltk.corpus import stopwords
+from nltk import sent_tokenize
+from nltk import word_tokenize
 
 
 # Create your views here.
@@ -30,15 +33,22 @@ def voice_recorder(request):
 
     wavio.write("myrecording.wav", recording, freq, sampwidth=2)
 
+    # speech_recognition
     spoken_text = speech_recognition(language)
 
-    words_list = re.sub("[^\w]", " ", spoken_text).split()
+    words_list = get_searched_words_and_sentence(spoken_text)
+    # words_list = (re.sub("[^\w]", " ", spoken_text).split())
+    # words_list=filter_clean_text(spoken_text)
+    # print(words_list)
+
+    # sent_list=sent_tokenize(spoken_text)
+
     links_list = []
+
     for i in words_list:
-        # temp_string += " " + "\n  " + i + " "
         links = list(search(i, tld="com", num=10, stop=10, pause=3))
         links_list += links
-
+    print(words_list)
     return render(request, 'test.html', {'spoken_text': spoken_text, 'links': links_list})
 
 
@@ -49,9 +59,9 @@ def speech_recognition(language):
         audio = r.record(source)
     mytext = ''
     try:
-        if(language=='Deutsch'):
-          mytext = r.recognize_google(audio, language='de-DE')
-        elif((language=='English')):
+        if (language == 'Deutsch'):
+            mytext = r.recognize_google(audio, language='de-DE')
+        elif ((language == 'English')):
             mytext = r.recognize_google(audio)
     except Exception as exp:
         print(exp)
@@ -64,7 +74,36 @@ def speech_recognition(language):
     # num : anzahl von links
     # stop : anzahl wo programm aufhoert
     # pause : warten
-
-
 def get_links_from_google(query):
     return search(query, tld="com", num=10, stop=10, pause=3)
+
+
+# here to filter the text from stoped word that is not important like ..to..on ..for.. from ...... .
+# function will return a list
+def filter_clean_text(spoken_text):
+    unwanted_words = set(stopwords.words("english") or stopwords.words("german"))
+    wanted_words = []
+    #     = [x for x in word_tokenize(spoken_text) if x not in unwanted_words]
+
+    for word in word_tokenize(spoken_text):
+        if word not in unwanted_words:
+            wanted_words.append(word)
+
+    return wanted_words
+
+
+# will return List of the most commend words in the text and complete sentence from the text
+def get_searched_words_and_sentence(spoken_text):
+    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+    most_commen_wordslist = []
+    temp_test = filter_clean_text(spoken_text)
+
+    for i in temp_test:
+        temp = temp_test.count(i)
+        if temp >= 2 and i not in punctuations:
+            most_commen_wordslist.append(i)
+    most_commen_wordslist = list(set(most_commen_wordslist))
+
+    sentence_list = sent_tokenize(spoken_text)
+
+    return most_commen_wordslist + sentence_list
