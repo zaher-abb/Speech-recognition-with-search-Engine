00349c68685d4ac1f631ab39b4de5995cc8918bf
zaher-abb <abboudz@th-brandenburg.de>
2021-06-02 16:07:27 +0200

    add Twitter lead user function

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,8 +8,10 @@ app_name = 'speech_recognition'
 
 urlpatterns = [
     path('HomePage', views.view_homePage, name='voice_recorder_HomePage'),
-    # TODO Delete
-    # path('voice_topic_result', views.fetch_topic_result, name='voice_topic_result'),
     path('voice_recorde_result', views.fetch_voice_recorde_result, name='voice_recorde_result'),
     path('addTopic', views.add_topic_and_fetch_topic_result, name='addTopic'),
+    path('get_lead_user',views.get_lead_user,name='get_lead_user'),
+    path('viewleaduser',views.view_leaduser,name='viewpager'),
+    path('get_lead_user_HttpResponse', views.get_lead_user_HttpResponse, name='get_lead_user_HttpResponse'),
+
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -2,14 +2,18 @@ from urllib import request
 from urllib.request import urlopen
 
 from bs4 import BeautifulSoup
+from django.http import HttpResponse
 from django.shortcuts import render
 import sounddevice as sd
 import speech_recognition as sr
 import wavio
 from googlesearch import search
 from nltk.corpus import stopwords
+from google_trans_new import google_translator
 
-
+import gzip
+import json
+import pandas as df
 from nltk import sent_tokenize
 from nltk import word_tokenize
 
@@ -45,7 +49,8 @@ def start_recording(request):
 
 def speech_recognition(language):
     r = sr.Recognizer()
-    audio = sr.AudioFile('myrecording.wav')
+
+    audio = sr.AudioFile('recording.wav')
     with audio as source:
         audio = r.record(source)
     text = ''
@@ -66,7 +71,7 @@ def get_links_from_google(searched_text, request):
     searched_word = searched_text[-1]
     if topic_list.count(searched_word) == 1 :
 
-        links = search(searched_word, tld="com", num=10, stop=5, pause=3)
+        links = search(searched_word, tld="com", num=10, stop=10, pause=4)
         for i in links :
            static_links_list.append(i)
         return static_links_list
@@ -122,24 +127,23 @@ def get_searched_words_and_sentence(spoken_text):
 
     return most_common_wordlist + sentence_list
 
-# TODO: Delete
-#
-# def fetch_topic_result(request):
-#     spoken_text = list(set(topic_list))
-#     links_list = get_links_from_google(spoken_text, request)
-#
-#     return render(request, 'test.html', {'spoken_text': spoken_text, 'links': links_list})
-#
 
 # add topic to the Static list  " topic_list " and fetch result by rendering the page
 def add_topic_and_fetch_topic_result(request):
-    topic_list.append(request.GET['topic'])
+
+    searched_word=request.GET['topic']
+    topic_list.append(searched_word)
     spoken_text = list(set(topic_list))
     links_list = get_links_from_google(topic_list, request)
-    print(stopwords.words("german"))
-    list(links_list)
+    links_list=list(links_list)
+    lead_user_Daten=get_lead_user(request)
 
-    return render(request, 'voice.html', {'spoken_text': spoken_text, 'links': links_list})
+    name_list=lead_user_Daten['name']
+
+    user_Score_list=lead_user_Daten['userScore']
+    zibList=zip(links_list,name_list,user_Score_list)
+
+    return render(request, 'voice.html', {'spoken_text': spoken_text,'zibList':zibList })
 
 
 # Implement a for loop and pass a list containing the string tags script and style into the Beautiful Soup object as the sequence. with  bs4.BeautifulSoup.decompose().
@@ -151,3 +155,66 @@ def read_text_of_website(url):
         script.decompose()
 
     return ' '.join(list(soup.stripped_strings))
+
+
+def get_lead_user(request):
+    searched_topic=request.GET['topic']
+
+
+    with gzip.open('couchdb_a_src_twitter_2021_04_08.json.gz', 'r') as zipfile:
+        twitter_data = json.loads(zipfile.read().decode('ascii'))
+    lead_users = []
+
+    for i in twitter_data:
+        try:
+            hashtags = []
+            for hashtag in i['entities']['hashtags']:
+                hashtags.append(hashtag['text'])
+            if searched_topic in i['text'] or searched_topic in hashtags:
+                lead_users.append(
+                    [i['user']['name'], i['user']['followers_count'], i['text'], i['user']['url'], i['retweet_count']])
+
+        except Exception as e:
+            print(e)
+            pass
+
+    tweet_text = df.DataFrame(data=lead_users,
+                              columns=['user', "followers_total", 'text', 'url', 'retweet_count'])
+
+    # Sum_Retweets
+    tweet_text['sum_retweet'] = tweet_text.groupby(['user'])['retweet_count'].transform('sum')
+
+    # number_of_tweets
+    tweet_text['number_of_tweets'] = tweet_text.groupby(['user'])['user'].transform('count')
+
+    # Drop the duplicates
+    tweet_text = tweet_text.drop_duplicates(subset=['user'])
+
+     # find a user_Score from formula
+    tweet_text['User_Score'] = 100000 * tweet_text['number_of_tweets'] \
+                               + 100000 * tweet_text['sum_retweet'] + 1 * tweet_text['followers_total']
+
+    # sorted list first into User_Score and then to followers number
+    sorted_list_for_leadUser = tweet_text.sort_values(by=['User_Score', 'followers_total'], ascending=False)
+
+    name=sorted_list_for_leadUser['user'].tolist()
+    userScore=sorted_list_for_leadUser['User_Score'].tolist()
+    followers_total=sorted_list_for_leadUser['followers_total'].tolist()
+    retweet_count=sorted_list_for_leadUser['retweet_count'].tolist()
+    text=sorted_list_for_leadUser['text'].tolist()
+    url=sorted_list_for_leadUser['url'].tolist()
+    pydict={'name':name,'userScore':userScore , 'followers_total' : followers_total , 'url' :url , 'retweet_count':retweet_count,'text':text}
+
+
+    return pydict
+#
+#
+def get_lead_user_HttpResponse(request):
+    lead_users_list_HttpResponse=get_lead_user(request)
+    frame= df.DataFrame(lead_users_list_HttpResponse)
+    geeks_object = frame.to_html()
+
+    return HttpResponse(geeks_object)
+
+def view_leaduser(request):
+    return render(request, 'twitter_lead_user.html')
